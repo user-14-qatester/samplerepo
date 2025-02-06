f1414387757d4fc413de8bc61c0b2bd6e3b612f3
HP <HP@NAA>
2025-02-06 12:06:46 +0530

    debugging commit.

---
+++ samplemaven/src/main/java/debuggingexample/Debugging1.java
@@ -0,0 +1,16 @@
+package debuggingexample;
+
+public class Debugging1 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+     int i;
+     for(i=0;i<=10;i++)
+     {
+    	 System.out.println(i);
+     }
+	}
+
+}
